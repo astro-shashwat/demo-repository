9b96cce89c5d6c6bbfa7e0d9d36a05c9f3327177
Shashwat Srivastava <158078343+astro-shashwat@users.noreply.github.com>
2025-09-17 12:49:10 +0530

    Demo Code

---
+++ Stellar_classification_ML.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+n = 1000
+
+fibonacci = [0,1]  # starting values
+
+for i in range(2, n):
+    fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+
+print(fibonacci[:20])
